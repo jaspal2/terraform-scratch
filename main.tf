b67977deaf2246c518c7cb3ed50087405a87c47e
jaspal singh <jaspal.singh@monash.edu>
2024-08-01 15:51:01 +1000

    Adding vpc and sg

--- main.tf
+++ main.tf
@@ -16,21 +16,48 @@ data "aws_ami" "ubuntu" {
 
 
 
-module "vpc" {
+module "vpc_from_scratch" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = var.vpc_name
   cidr = "10.0.0.0/16"
 
- 
+
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false  #deploy NAT
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+
+
+module "SG_from_scratch" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = module.vpc_from_scratch.id
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp", "  http-80-tcp", "http-8080-tcp",  https-8443-tcp]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 }
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "vpc_name" {
+    default   =     "terraform_scratch_vpc"
+}
